5a542ff23fb6a5796c29a86a139a6270b54e8293
Reffotsirk Nged <kristoffer@hylleby.dk>
2017-10-31 19:54:48 +0100

    sadasd

--- src/Main.hs
+++ src/Main.hs
@@ -3,3 +3,11 @@ module Main where
 main :: IO ()
 main = do
   putStrLn "hello world"
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+isPrime :: Integer -> Bool
+isPrime 1 = True
